6bb829683f335e405cf069ef5eef5ba028e7ba0a
Yago Vega <yagofvv@gmail.com>
2018-04-23 19:54:03 +0200

    index work

--- bin/plans_data.js
+++ bin/plans_data.js
@@ -5,6 +5,25 @@ module.exports = [
     creator: "5addcee3b8e47e3e878993d0",
     genre: "Outdoors",
     assistants: [
+      "5addcee3b8e47e3e878993d0",
+      "5addf03f2ae0d74d4d170a43"
+    ]
+  },
+  {
+    title: "Concierto de Le Marc",
+    description: "Conciertazo por la noche flipas pipas",
+    creator: "5addf13cbe49e84df7b15fb4",
+    genre: "Outdoors",
+    assistants: [
+      "5addcee3b8e47e3e878993d0"
+    ]
+  },
+  {
+    title: "Juego de mesa super guay",
+    description: "Quedamos en mi casa para jugar a un juego de mesa super guachi",
+    creator: "5addf03f2ae0d74d4d170a43",
+    genre: "Outdoors",
+    assistants: [
       "5addcee3b8e47e3e878993d0"
     ]
   }
