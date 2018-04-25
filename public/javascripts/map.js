1842bad482af08a46d4e10c572eb18d4e6630224
Raul.dll <rdl.llave@gmail.com>
2018-04-25 20:04:31 +0200

    timeiscoming

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,82 +1,84 @@
 window.onload = () => {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 43.392513,
-      lng: -3.698211
-    },
-    zoom: 15
-  });
-
-  //Marks
-  let bounds = new google.maps.LatLngBounds();
-  let changedUse = window.plans.map(plan => {
-      console.log(plan)
-    let mappeduse = {
-      title: plan.title,
-      pos: {
-        lat: plan.location.coordinates[0],
-        lng: plan.location.coordinates[1]
+  if (document.getElementById("map")) {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: 43.392513,
+        lng: -3.698211
       },
-      id: plan._id,
-      date: plan.date,
-      genre: plan.genre,
-      subgenre: plan.subgenre
-    };
-    //console.log(plan.username)
-    bounds.extend(mappeduse.pos);
-    return mappeduse;
-  });
-  map.fitBounds(bounds);
+      zoom: 15
+    });
 
-  changedUse.forEach(plan => {
-    let infowindow = new google.maps.InfoWindow({
-      content: `<h3>${plan.title}</h3>
+    //Marks
+    let bounds = new google.maps.LatLngBounds();
+    let changedUse = window.plans.map(plan => {
+      console.log(plan);
+      let mappeduse = {
+        title: plan.title,
+        pos: {
+          lat: plan.location.coordinates[0],
+          lng: plan.location.coordinates[1]
+        },
+        id: plan._id,
+        date: plan.date,
+        genre: plan.genre,
+        subgenre: plan.subgenre
+      };
+      //console.log(plan.username)
+      bounds.extend(mappeduse.pos);
+      return mappeduse;
+    });
+    map.fitBounds(bounds);
+
+    changedUse.forEach(plan => {
+      let infowindow = new google.maps.InfoWindow({
+        content: `<h3>${plan.title}</h3>
       <p>${plan.date}</p>
       <a href="/plans/${plan.genre}/${plan.subgenre}/${plan.id}">Go to plan</a>`
+      });
+
+      let marker = new google.maps.Marker({
+        position: plan.pos,
+        map: map,
+        title: plan.name
+      });
+      marker.addListener("click", function() {
+        infowindow.open(map, marker);
+      });
     });
 
-    let marker = new google.maps.Marker({
-      position: plan.pos,
-      map: map,
-      title: plan.name
+    var infoWindow = new google.maps.InfoWindow({
+      map: map
     });
-    marker.addListener("click", function() {
-      infowindow.open(map, marker);
-    });
-  });
-
-  var infoWindow = new google.maps.InfoWindow({
-    map: map
-  });
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
       console.log("jesus");
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+      navigator.geolocation.getCurrentPosition(
+        function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Usted esta Aquí");
-        map.setCenter(pos);
-      },
-      function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Usted esta Aquí");
+          map.setCenter(pos);
+        },
+        function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+    }
   }
 };
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,60 +1,33 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    const labels = window.plans.map(e => e._id);
-    const data = window.plans.map(e => e.total);
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-      type: "pie",
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            data: data,
-            backgroundColor: ["#FF463B", "#F0FF54", "#1B76CC"],
-            borderColor: [
-              "rgba(255,99,132,1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)"
-            ],
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        legend: { display: false }
-      }
-    });
-
-    const ironhackMAD = { lat: 40.392491, lng: -3.697986 };
-    // Map initialization
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: ironhackMAD
-    });
-
-    const myPlaces = window.places;
-    console.log(myPlaces);
-
-    // Add restaurant markers to map
-    let markers = [];
-    myPlaces.forEach(function(place) {
-      let title = place.name;
-      let position = {
-        lat: place.location.coordinates[0],
-        lng: place.location.coordinates[1]
-      };
-      var pin = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: title
+    if (window.plans) {
+      const labels = window.plans.map(e => e._id);
+      const data = window.plans.map(e => e.total);
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              data: data,
+              backgroundColor: ["#FF463B", "#F0FF54", "#1B76CC"],
+              borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)"
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          legend: { display: false }
+        }
       });
-      markers.push(pin);
-    });
-    //  var bounds = new google.maps.LatLngBounds();
-    // bounds.extend(myPlaces);
-    // map.fitBounds(bounds);
+    }
   },
   false
 );
