4c2a0743b83a28d826664b421fb9ba34f3d7b8a5
Raul.dll <rdl.llave@gmail.com>
2018-04-25 17:33:07 +0200

    otro

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
+hbs.registerPartials(__dirname + '/views/plans/partials');
 hbs.registerHelper("ifUndefined", (value, options) => {
   if (arguments.length < 2)
     throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
@@ -80,6 +81,7 @@ app.use((req,res,next) =>{
   res.locals.user = req.user;
   next();
 })
+
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
