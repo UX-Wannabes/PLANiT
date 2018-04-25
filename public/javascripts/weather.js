f04412e2ddb37c8c2f3fe52af96a1cd191301780
Yago Vega <yagofvv@gmail.com>
2018-04-25 20:19:51 +0200

    asiofjlksfaj

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded",() => {
   document.getElementById('date').onchange = function(){
-    console.log('i')
-    let address = document.getElementById('adress').value
-    address = address.replace(/\s+/g,'+')
+    let address = document.getElementById('adress').value;
+    address = address.replace(/\s+/g,'+');
     console.log(address)
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCgsMC659ayWxa7vhbI_ykWqzdavn8ZOxg`)
     .then(function (response) {
-      console.log(response)
-      console.log(response.data.results[0].geometry.location)
-      let lat = response.data.results[0].geometry.location.lat
-      let long = response.data.results[0].geometry.location.lng
+      let lat = response.data.results[0].geometry.location.lat;
+      let long = response.data.results[0].geometry.location.lng;
       axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=a68b6bdea8f65c2b91544b4501750026`).then(w=>{
         console.log(w)
+        document.getElementById("raul").innerHTML+=`<p>${w.data.list[0].weather[0].main}</p> <img src="http://openweathermap.org/img/w/${w.data.list[0].weather[0].icon}.png">`
       })
     })
     .catch(function (error) {
